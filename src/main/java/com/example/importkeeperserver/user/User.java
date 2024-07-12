87556621a3c898b5dc16513881c03995da36c531
JB <wjdqls6704@naver.com>
2024-07-13 03:20:12 +0900

    feat: User 클래스 작성

---
+++ src/main/java/com/example/importkeeperserver/user/User.java
@@ -0,0 +1,25 @@
+package com.example.importkeeperserver.user;
+
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "users")
+@Getter
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String name;
+
+    @Builder
+    public User(String name){
+        this.name = name;
+    }
+}
