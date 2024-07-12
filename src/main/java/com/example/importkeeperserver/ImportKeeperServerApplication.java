87f559a07b9cb9c4bf8961cf1c7c15792f2c56ff
JB <wjdqls6704@naver.com>
2024-07-13 01:23:22 +0900

    setting

---
+++ src/main/java/com/example/importkeeperserver/ImportKeeperServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.importkeeperserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImportKeeperServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImportKeeperServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/importkeeperserver/ImportKeeperServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.importkeeperserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImportKeeperServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
