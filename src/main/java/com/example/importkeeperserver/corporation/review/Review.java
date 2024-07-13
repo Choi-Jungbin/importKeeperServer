5b2eda2490d5d6831b6aabbb60909b73c9a8380b
JB <73532407+Choi-Jungbin@users.noreply.github.com>
2024-07-13 15:00:26 +0900

    feat: Review 클래스 작성

---
+++ src/main/java/com/example/importkeeperserver/corporation/review/Review.java
@@ -0,0 +1,35 @@
+package com.example.importkeeperserver.corporation.review;
+
+import com.example.importkeeperserver.corporation.Corporation;
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "reviews")
+@Getter
+@NoArgsConstructor
+public class Review {
+
+    @Id
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "corp_id")
+    private Corporation corporation;
+
+    @Column(nullable = false)
+    private int rating;
+
+    @Column(columnDefinition = "longtext")
+    private String review;
+
+    @Builder
+    public Review(Long id, Corporation corporation, int rating, String review){
+        this.id = id;
+        this.corporation = corporation;
+        this.rating = rating;
+        this.review = review;
+    }
+}
