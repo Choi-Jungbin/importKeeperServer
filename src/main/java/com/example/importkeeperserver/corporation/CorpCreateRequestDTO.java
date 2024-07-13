63c096a318b399f8a8f313e429cf3e40f5664b76
JB <73532407+Choi-Jungbin@users.noreply.github.com>
2024-07-13 16:43:56 +0900

    feat: CorpCreateRequestDTO 추가

--- src/main/java/com/example/importkeeperserver/corporation/CorpCreateRequestDTO.java
+++ src/main/java/com/example/importkeeperserver/corporation/CorpCreateRequestDTO.java
@@ -13,6 +13,15 @@ public class CorpCreateRequestDTO {
     private Country country;
     private Map<String, reviewDTO> reviews;
 
+    public CorpCreateRequestDTO(String id, String name,
+                                Country country,
+                                Map<String, reviewDTO> reviews){
+        this.id = id;
+        this.name = name;
+        this.country = country;
+        this.reviews = reviews;
+    }
+
     @Getter
     public static class reviewDTO{
         @NotEmpty
