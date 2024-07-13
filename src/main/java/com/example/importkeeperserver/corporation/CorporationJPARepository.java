43d1acb1ab4f09ebaad9a81787e75301571f7705
JB <73532407+Choi-Jungbin@users.noreply.github.com>
2024-07-13 15:18:51 +0900

    feat: CorporationJPARepository 작성

---
+++ src/main/java/com/example/importkeeperserver/corporation/CorporationJPARepository.java
@@ -0,0 +1,6 @@
+package com.example.importkeeperserver.corporation;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CorporationJPARepository extends JpaRepository<Corporation, Long> {
+}
