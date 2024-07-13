5d5194bc7ae8cbebe43df4b39543c49cce10031e
JB <73532407+Choi-Jungbin@users.noreply.github.com>
2024-07-13 15:19:08 +0900

    feat: ReviewJPARepository 작성

---
+++ src/main/java/com/example/importkeeperserver/corporation/review/ReviewJPARepository.java
@@ -0,0 +1,6 @@
+package com.example.importkeeperserver.corporation.review;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ReviewJPARepository extends JpaRepository<Review, Long> {
+}
